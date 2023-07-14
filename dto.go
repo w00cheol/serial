214febeb3e45965b611ba81a56bff53d9c88e85f
w00cheol <kw8384@gmail.com>
2023-07-14 09:36:43 +0900

    Feat(dto.go)
    add AllData struct
    - define new data type to send all data

--- dto.go
+++ dto.go
@@ -24,3 +24,16 @@ type SoundData struct {
 	Peak [6]int64
 	Amp  [6]float64
 }
+
+type AllData struct {
+	Temperature float64
+	Humidity    float64
+	Pressure    float64
+	Tilt        TiltData
+	VibrationX  VibrationData
+	VibrationY  VibrationData
+	VibrationZ  VibrationData
+	Light       int8
+	Sound       SoundData
+	Broadband   float64
+}
